fe9de85d43918b8ba3b14bb08a3d5dce872d6768
Nataliesmyth <nataliesmyth11@gmail.com>
2024-04-03 19:10:38 -0400

    calculated X and Y fractional values for mouse location

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 let input = {
     mouseX: {
-        start: 100,
-        end: window.innerWidth - 200,
+        start: 0,
+        end: window.innerWidth,
         current: 0,
     },
-    mouseY: {}
+    mouseY: {
+        start: 0,
+        end: window.innerHeight,
+        current: 0,
+    }
 };
 
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
+input.mouseY.range = input.mouseY.end - input.mouseY.start;
 
 let handleMouseMove = function (event) {
     // current value
     input.mouseX.current = event.clientX;
+    input.mouseY.current = event.clientY;
     // fractional value
     input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
-    if (input.mouseX.fraction > 1) {
-        input.mouseX.fraction = 1;
-    }
-    if (input.mouseX.fraction < 0) {
-        input.mouseX.fraction = 0;
-    }
-    console.log('fraction', input.mouseX.fraction)
+    input.mouseY.fraction = (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
+    // if (input.mouseX.fraction > 1) {
+    //     input.mouseX.fraction = 1;
+    // }
+    // if (input.mouseX.fraction < 0) {
+    //     input.mouseX.fraction = 0;
+    // }
+    // if (input.mouseY.fraction > 1) {
+    //     input.mouseY.fraction = 1;
+    // }
+    // if (input.mouseY.fraction < 0) {
+    //     input.mouseY.fraction = 0;
+    // }
+    console.log('fraction X', input.mouseX.fraction)
+    console.log('fraction Y', input.mouseY.fraction)
 }
 let handleResize = function () {
-    input.mouseX.end = window.innerWidth - 200;
+    input.mouseX.end = window.innerWidth;
     input.mouseX.range = input.mouseX.end - input.mouseX.start;
+    input.mouseY.end = window.innerWidth;
+    input.mouseY.range = input.mouseX.end - input.mouseY.start;
 }
 
 
